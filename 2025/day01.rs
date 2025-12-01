357ad3b4173250268761c65c417ba117afb812ce
toino <me@toino.pt>
2025-12-01 12:31:51 +0000

    2025 day 1 solution

---
+++ 2025/day01.rs
@@ -0,0 +1,46 @@
+use std::io;
+
+fn get_input() -> Vec<isize> {
+    io::stdin()
+        .lines()
+        .map(|l| {
+            let mut line = l.unwrap();
+            let num = line.split_off(1);
+            num.parse::<isize>().unwrap() * if line == "L" { -1 } else { 1 }
+        })
+        .collect()
+}
+
+fn part1(input: &Vec<isize>) -> isize {
+    input
+        .iter()
+        .scan(50, |total, i| {
+            *total = (*total + i).rem_euclid(100);
+            Some(if *total == 0 { 1 } else { 0 })
+        })
+        .sum()
+}
+
+fn part2(input: &Vec<isize>) -> isize {
+    input
+        .iter()
+        .scan(50, |total, i| {
+            *total = *total + i;
+            let mut result = (*total / 100).abs();
+            if *i < 0 && *total <= 0 && total != i {
+                result += 1
+            };
+            *total = total.rem_euclid(100);
+            Some(result)
+        })
+        .sum()
+}
+
+fn main() {
+    let input = get_input();
+
+    let result1 = part1(&input);
+    println!("Part 1: {result1}");
+    let result2 = part2(&input);
+    println!("Part 2: {result2}");
+}
